fcc6856f084eb9283fb723d2ff5286a5da568b1f
panseri.21255 <panseri.tommaso.studente@itispaleocapa.it>
2024-10-14 12:19:18 +0200

    commit1 commit2 commit3 commit4

--- src/giocatore/Main.java
+++ src/giocatore/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
         Scanner in = new Scanner (System.in);
         int scelta;
+        String nome = " ";
+        String capitano = " ";
+        int goal = 0;
+
         Giocatore [] g = new Giocatore[11];
         Giocatore g1 = new Giocatore();
         System.out.println ("Nome del giocatore 1: "); g1.setNome(in.nextLine());
@@ -27,6 +31,10 @@ public class Main {
             scelta = in.nextInt();
             switch (scelta){
                 case 1:
+                    System.out.println ("Nome del giocatore 2: ");
+                    System.out.println ("Il giocatore 2 è capitano?: ");
+                    System.out.println ("Goal giocatore 2: ");
+                    in.nextLine();
                     
             }
 
@@ -36,66 +44,17 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        Giocatore g2 = new Giocatore();
-        int conta = 0;
-
-        System.out.println ("Nome del giocatore 1: "); g1.setNome(in.nextLine());
-        System.out.println ("Il giocatore 1 è capitano?: "); g1.setCapitano(in.nextLine());
-        System.out.println ("Goal giocatore 1: "); g1.setGoal(in.nextInt());
-        in.nextLine();
-
         System.out.println ("Nome del giocatore 2: "); g2.setNome(in.nextLine());
         System.out.println ("Il giocatore 2 è capitano?: "); g2.setCapitano(in.nextLine());
         System.out.println ("Goal giocatore 2: "); g2.setGoal(in.nextInt());
         in.nextLine();
 
-        while (true) {
-            System.out.println("0 - Inserisci giocatore");
-            System.out.println("1 - Mostra squadra");
-            System.out.println("2 - Modifica giocatore");
-            System.out.println("3 - Rimuovi giocatore");
-            System.out.println("4 - Visualizza giocatori con almeno 5 goal");
-            System.out.println("5 - Visualizza capitano");
-            System.out.println("6 - Assegna capitano casualmente");
-            System.out.println("7 - Esci");
-            System.out.print("Scelta: ");
-            int scelta = input.nextInt();
-            switch (scelta) {
-                case 0:
-
-            }
-
         g[0] = g1;
-
-        System.out.println("Vuoi aggiungere un giocatore?: ");
-        String risposta = in.nextLine();
-
-        if (risposta.equals("si") || risposta.equals("Si")){
-            if (g1.getCapitano().equals("no")){
-                conta++;
-
-                g[conta] = g2;
-            }
-        }
 
 
     }
 
-    private static
+    private static void aggiuntaGiocatore (String nomeNuovo, int goalNuovo, String capitanoNuovo, int indice){
+            g[indice] = Giocatore(nomeNuovo, goalNuovo, capitanoNuovo);
+        }
 }
